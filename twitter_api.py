780d75dcc513c23bcc5a5a1f03ae136dcb2df7ee
Shaik Suhail Ahmed <84620891+suhail-3206@users.noreply.github.com>
2022-12-10 20:49:13 +0530

    Add files via upload

---
+++ twitter_api.py
@@ -0,0 +1,33 @@
+import tweepy
+import configparser
+import pandas as pd
+
+# read configs
+config = configparser.ConfigParser()
+config.read('confing.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+# authentication
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+keywords = '#DigitalIndia'
+limit=5000
+tweets = tweepy.Cursor(api.search_tweets, q=keywords, count=100, lang="en",tweet_mode='extended').items(limit)
+# create DataFrame
+columns = ['User', 'Tweet']
+data = []
+for tweet in tweets:
+    data.append([tweet.user.screen_name, tweet.full_text])
+
+df = pd.DataFrame(data, columns=columns)
+print(df)
+#csv conversion
+df.to_csv('tweets.csv')
